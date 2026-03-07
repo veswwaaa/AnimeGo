b86371fa806efc8c4fd14954b8cb4cf061c6adef
veswwaaa <vanoaji402@gmail.com>
2026-03-07 16:41:34 +0800

    imseng saja ye

---
+++ lib/models/model_anime.dart
@@ -0,0 +1,33 @@
+class DataAnim {
+  final String title;
+  final String japaneseTitle;
+  final String synopsis;
+  final String imageUrl;
+  final String score;
+  final String genres;
+  final String sourceUrl;
+
+
+  DataAnim({
+    required this.title,
+    required this.japaneseTitle,
+    required this.synopsis,
+    required this.imageUrl,
+    required this.score,
+    required this.genres,
+    required this.sourceUrl,
+  });
+
+
+  factory DataAnim.fromMap(Map<String, dynamic> map) {
+    return DataAnim(
+      title: map['title'] ?? '',
+      japaneseTitle: map['japaneseTitle'] ?? '',
+      synopsis: map['synopsis'] ?? '',
+      imageUrl: map['images'] ? ['jpg'] ? ['image_url'] ?? '',
+      score: (map['score'] ?? 0).toDouble(),
+      genres: List<String>.from(map['genres'] ?? []).join(', '),
+      sourceUrl: map['sourceUrl'] ?? '',
+    );
+  }
+}
